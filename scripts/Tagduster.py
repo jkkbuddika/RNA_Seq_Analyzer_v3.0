c5dc6bc252fd9bac874063a3391efb492c2b1ac0
Kasun Buddika <jkkbuddika@gmail.com>
2020-06-02 15:32:36 -0400

    Code adjustment

--- scripts/Tagduster.py
+++ scripts/Tagduster.py
@@ -5,9 +5,8 @@ import ColorTextWriter
 
 class Tagduster:
 
-    def __init__(self, home_dir, threads, tagdust_sing, input_dir, rrna, extensions, seq_method):
+    def __init__(self, home_dir, tagdust_sing, input_dir, rrna, extensions, seq_method):
         self.home_dir = home_dir
-        self.threads = threads
         self.tagdust_sing = tagdust_sing
         self.input_dir = input_dir
         self.rrna_list = rrna
@@ -19,25 +18,25 @@ class Tagduster:
         outdir = os.path.join(self.home_dir, 'tagdust_out')
         if not os.path.isdir(outdir): os.mkdir(outdir)
 
-        r1_reads = sorted(glob.glob(self.input_dir + '*R1_trimmed_UMI.fastq'))
-        r2_reads = sorted(glob.glob(self.input_dir + '*R2_trimmed_UMI.fastq'))
+        r1_reads = sorted(glob.glob(self.input_dir + '*R1_trimmed.fastq'))
+        r2_reads = sorted(glob.glob(self.input_dir + '*R2_trimmed.fastq'))
 
         ctw = ColorTextWriter.ColorTextWriter()
 
         print('\n' + ctw.CBEIGE + ctw.CBOLD + 'Running TagDust ...' + ctw.CEND)
 
         if self.seq_method == 'single':
-            for i in r2_reads:
+            for i in r1_reads:
                 print('\n' + ctw.CBEIGE + ctw.CBOLD + 'Tagdusting: ' + ctw.CBLUE + os.path.basename(i) + ctw.CBEIGE + ctw.CBOLD + ' ...' + ctw.CEND + '\n')
 
-                output_file = outdir + '/' + os.path.basename(i).split('_R2_trimmed_UMI.fastq')[0] + '_tagdustout'
+                output_file = outdir + '/' + os.path.basename(i).split('_trimmed')[0] + '_tagdustout'
 
                 command = [
                     'module load singularity;singularity exec -e -C -B', self.home_dir,
                     '-H', self.home_dir, self.tagdust_sing,
-                    'tagdust -t', self.threads,
-                    '-1 O:N -2 R:N', '-o', output_file,
-                    '-ref', self.rrna_list, '-fe 2', i
+                    'tagdust -1 R:N',
+                    '-o', output_file,
+                    '-ref', self.rrna_list, '-fe 0', i
                 ]
 
                 command = ' '.join(command)
@@ -45,19 +44,20 @@ class Tagduster:
 
         elif self.seq_method == 'paired':
             for (i, j) in zip(r1_reads, r2_reads):
-                print('\n' + ctw.CBEIGE + ctw.CBOLD + 'Tagdusting: ' + ctw.CBLUE + os.path.basename(i) + ' and ' + os.path.basename(j) + ctw.CBEIGE + ctw.CBOLD + ' ...' + ctw.CEND + '\n')
+                if i.endswith('R1_trimmed.fastq') and j.endswith('R2_trimmed.fastq'):
+                    print('\n' + ctw.CBEIGE + ctw.CBOLD + 'Tagdusting: ' + ctw.CBLUE + os.path.basename(i) + ' and ' + os.path.basename(j) + ctw.CBEIGE + ctw.CBOLD + ' ...' + ctw.CEND + '\n')
 
-                output_file = outdir + '/' + os.path.basename(i).split('_R1_trimmed_UMI.fastq')[0] + '_tagdustout'
+                    output_file = outdir + '/' + os.path.basename(i).split('_R1_trimmed')[0] + '_tagdustout'
 
-                command = [
-                    'module load singularity;singularity exec -e -C -B', self.home_dir,
-                    '-H', self.home_dir, self.tagdust_sing,
-                    'tagdust -t', self.threads,
-                    '-1 O:N -2 R:N', '-o', output_file,
-                    '-ref', self.rrna_list, '-fe 2', i, j
-                ]
+                    command = [
+                        'module load singularity;singularity exec -e -C -B', self.home_dir,
+                        '-H', self.home_dir, self.tagdust_sing,
+                        'tagdust -1 R:N',
+                        '-o', output_file,
+                        '-ref', self.rrna_list, '-fe 0', i, j
+                    ]
 
-                command = ' '.join(command)
-                sp.check_call(command, shell=True)
+                    command = ' '.join(command)
+                    sp.check_call(command, shell=True)
 
         print('\n' + ctw.CRED + ctw.CBOLD + 'Running TagDust Completed!!!' + ctw.CEND + '\n')
