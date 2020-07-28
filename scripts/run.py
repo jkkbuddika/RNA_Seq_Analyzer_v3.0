df4b204d7d197bb3a32083c160f0ee83d20ca05f
Kasun Buddika <jkkbuddika@gmail.com>
2020-07-27 22:00:02 -0400

    Update script

--- scripts/run.py
+++ scripts/run.py
@@ -1,12 +1,10 @@
-import GeneralVariables
-import CommonVariables
+import UserDefinedOptions
+import CommonOptions
 import FastQCRunner
-import Tagduster
-import TDSummaryProcessor
 import CutAdapt
 import WebDownloader
 import RefGenMaker
-import StarAligner
+import GenomeAligner
 import SamTools
 import BigWigFileMaker
 import FeatureCounter
@@ -17,22 +15,15 @@ import ColorTextWriter
 
 ctw = ColorTextWriter.ColorTextWriter()
 print('\n' + ctw.CRED + ctw.CBOLD + 'Initiating data analysis ...' + ctw.CEND + '\n')
-print(ctw.CRED + 'This script can take minutes to hours to analyze your data based on the number of libraries to be analyzed ...' + '\n')
 
-gv = GeneralVariables.GeneralVariables()
-cv = CommonVariables.CommonVariables()
+gv = UserDefinedOptions.UserDefinedOptions()
+cv = CommonOptions.CommonOptions()
 
-qc_1 = FastQCRunner.FastQCRunner(cv.home_dir, cv.fastqc_raw, cv.raw_sequences_dir)
-qc_1.fastqc()
+qc = FastQCRunner.FastQCRunner(cv.home_dir, cv.fastqc_raw, cv.raw_sequences_dir)
+qc.fastqc()
 
-ca = CutAdapt.CutAdapt(cv.home_dir, cv.raw_sequences_dir, cv.extensions, gv.seq_method)
+ca = CutAdapt.CutAdapt(cv.home_dir, cv.raw_sequences_dir, cv.extensions, cv.seq_method)
 ca.cutadapt()
-
-td = Tagduster.Tagduster(cv.home_dir, cv.tagdust_singu, cv.cutadapt_dir, cv.rRNA_path, cv.extensions, gv.seq_method)
-td.tagdust()
-
-tdsp = TDSummaryProcessor.TDSummaryProcessor(cv.home_dir, cv.tagdust_out)
-tdsp.td_summary()
 
 wd = WebDownloader.WebDownloader(cv.home_dir, cv.genome_dir_name, cv.genome_path, cv.genome_file)
 wd.download()
@@ -43,16 +34,16 @@ wd.download()
 rg = RefGenMaker.RefGenMaker(cv.home_dir, cv.Threads, cv.genome_fa, cv.genes_gtf)
 rg.refgen()
 
-sa = StarAligner.StarAligner(cv.home_dir, cv.tagdust_out, cv.Threads, cv.ref_genome, cv.extensions, cv.genes_gtf, gv.seq_method)
+sa = GenomeAligner.GenomeAligner(cv.home_dir, cv.cutadapt_dir, cv.Threads, cv.ref_genome, cv.extensions, cv.genes_gtf, cv.seq_method)
 sa.aligner()
 
-ss = SamTools.SamTools(cv.home_dir, cv.star_aligned, cv.Threads, cv.extensions, gv.seq_method, gv.deduplication)
+ss = SamTools.SamTools(cv.home_dir, cv.star_aligned, cv.Threads, cv.extensions, cv.seq_method, gv.dedup)
 ss.sam_sorting()
 
 bw = BigWigFileMaker.BigWigFileMaker(cv.home_dir, cv.sam_sorted, cv.extensions)
 bw.bigwig()
 
-fc = FeatureCounter.FeatureCounter(cv.home_dir, cv.sam_sorted, gv.diff_features, gv.stranded, cv.feature_dir, cv.feature_file, cv.extensions, gv.seq_method)
+fc = FeatureCounter.FeatureCounter(cv.home_dir, cv.sam_sorted, gv.diff_features, cv.feature_dir, cv.feature_file, cv.extensions, cv.seq_method)
 fc.feature()
 
 mqc = MultiQCRunner.MultiQCRunner(cv.home_dir)
